258beec8ac00ae1497465c9e62c841fd3c6eadca
shekhar <shekharkubbireddy@gmail.com>
2025-09-21 18:08:48 +0530

    final commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,30 +1,14 @@
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-// declare global {
-//   // Allow global prisma for hot-reloading in development
-//   var prisma: PrismaClient | undefined
-// }
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-// const client = globalThis.prisma ?? new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query", "error", "warn"],
+  });
 
-// if (process.env.NODE_ENV !== 'production') {
-//   globalThis.prisma = client
-// }
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-// export default client
-
-import { PrismaClient } from '../generated/prisma'
-
-
-
-declare global {
-
-    var prisma: PrismaClient | undefined
-
-}
-
-export const client = globalThis.prisma || new PrismaClient()
-
-if(process.env.NODE_ENV !== "production") {
-    globalThis.prisma = client
-}
+// Export as client for backward compatibility
+export const client = prisma;
