3766fb6f647b2b145a3459fcbfd237ee9e11e52a
K.SOMA CHINNA SHEKHAR <shekharkubbireddy@gmail.com>
2026-04-01 22:48:50 +0530

    deploy change commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(process.cwd()),
   images: {
     domains: ['oaidalleapiprodscus.blob.core.windows.net'],
     dangerouslyAllowSVG: true,
--- src/app/(protected)/(pages)/(dashboardPages)/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-const DashboardIndexPage = () => {
-	redirect('/dashboard');
-};
-
-export default DashboardIndexPage;
